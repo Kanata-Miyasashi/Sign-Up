76344c788ef42b88388ee6122d9fdc4177a89bc5
Kanata <100531569+Kanata-Miyasashi@users.noreply.github.com>
2023-01-08 17:52:58 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+// Sign Up Function
+function signUp(){
+    var username = document.getElementById('username_input').value;
+    var email = document.getElementById('email_input').value;
+    var password = document.getElementById('password_input').value;
+
+    localStorage.setItem("Username: ", username);
+    localStorage.setItem("Email: ", email);
+    localStorage.setItem("Password: ", password);
+    goToLogin()
+    }
+
+
+    
+// Go to Login
+    function goToLogin(){
+        window.open("","_self")
+    }
